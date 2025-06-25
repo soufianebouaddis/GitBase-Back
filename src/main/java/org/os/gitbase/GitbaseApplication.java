f0a2745351cd3509ebee5c2917fe33eaef5ea3b2
soufianeBouaddis <fousenaibdisbdis@gmail.com>
2025-06-25 22:40:00 +0100

    # First commit GitBase Back

---
+++ src/main/java/org/os/gitbase/GitbaseApplication.java
@@ -0,0 +1,13 @@
+package org.os.gitbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/os/gitbase/GitbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.os.gitbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
